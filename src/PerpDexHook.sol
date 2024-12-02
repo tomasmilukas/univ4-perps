4913bc208983bd0b9d3d8febb46dab9cf57458ac
Tomas Milukas <tmilukas@gmail.com>
2024-12-02 23:23:43 +0100

    some additions

--- src/PerpDexHook.sol
+++ src/PerpDexHook.sol
@@ -3,15 +3,22 @@ pragma solidity 0.8.26;
 
 import {BaseHook} from "v4-periphery/src/base/hooks/BaseHook.sol";
 import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
+import {StateLibrary} from "v4-core/libraries/StateLibrary.sol";
+import {PoolIdLibrary} from "v4-core/types/PoolId.sol";
 import {Hooks} from "v4-core/libraries/Hooks.sol";
 import {PoolKey} from "v4-core/types/PoolKey.sol";
-import {Currency} from "v4-core/types/Currency.sol";
+import {Currency, CurrencyLibrary} from "v4-core/types/Currency.sol";
 import {BalanceDelta} from "v4-core/types/BalanceDelta.sol";
 import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import {AggregatorV3Interface} from "chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
-import "stringutils/strings.sol";
+import "solidity-stringutils/src/strings.sol";
 
 contract PerpDexHook is BaseHook {
+    using strings for *;
+    using StateLibrary for IPoolManager;
+    using PoolIdLibrary for PoolKey;
+    using CurrencyLibrary for Currency;
+
     uint256 public constant MAX_LEVERAGE = 5;
     uint256 public constant LIQUIDATION_THRESHOLD = 80; // 80%
     uint256 public constant BASE_LEVERAGE_RATE = 1e15; // 0.1% base leverage rate
@@ -21,17 +28,19 @@ contract PerpDexHook is BaseHook {
 
     struct Position {
         address trader;
-        uint256 size;
-        uint256 margin;
+        uint256 sizeUSD;
+        uint256 marginAmount;
+        address marginCurrency;
         uint256 leverage;
-        int256 entryPrice;
+        address currencyBettingOn;
+        uint256 entryPriceUSD;
         bool isLong;
         uint256 timestamp;
     }
 
     struct Collateral {
-        uint256 currency0;
-        uint256 currency1;
+        uint256 currency0Amount;
+        uint256 currency1Amount;
     }
 
     // Only one position per trader
@@ -39,16 +48,29 @@ contract PerpDexHook is BaseHook {
     mapping(address => Collateral) public traderCollateral;
 
     uint256 public totalLiquidity;
-    uint256 public lockedLiquidity;
+
+    // Locked currencies for traders realise profits.
+    uint256 public traderPayOutLockedCurrency0;
+    uint256 public traderPayOutLockedCurrency1;
+
     uint256 public longOpenInterest;
     uint256 public shortOpenInterest;
+
+    // LPs set aside 10% of their position as buffer capital for trader payouts so we avoid rebalancing LP positions
+    uint256 public bufferCapitalCurrency0;
+    uint256 public bufferCapitalCurrency1;
+
     // Fees for traders to divide between each other for imbalance
-    uint256 public imbalanceFees;
+    uint256 public fundingFeesCurrency0;
+    uint256 public fundingFeesCurrency1;
 
     // Fees for LPs to earn from renting leverage
-    uint256 public leverageFees;
+    uint256 public leverageFeesCurrency0;
+    uint256 public leverageFeesCurrency1;
+
     // Fees for LPs from traders losing/making money. Can be negative, aka LPs pay out fees.
-    uint256 public traderPnLFees;
+    uint256 public traderPnLFeesCurrency0;
+    uint256 public traderPnLFeesCurrency1;
 
     // Chainlink specifics
     AggregatorV3Interface public priceFeed;
@@ -97,10 +119,10 @@ contract PerpDexHook is BaseHook {
         uint160 sqrtPriceX96
     ) external override returns (bytes4) {
         totalLiquidity = 0;
-        lockedLiquidity = 0;
         longOpenInterest = 0;
         shortOpenInterest = 0;
-        imbalanceFees = 0;
+        fundingFeesCurrency0 = 0;
+        fundingFeesCurrency1 = 0;
 
         currency0Address = Currency.unwrap(key.currency0);
         currency1Address = Currency.unwrap(key.currency1);
@@ -138,10 +160,10 @@ contract PerpDexHook is BaseHook {
         IPoolManager.ModifyLiquidityParams calldata params,
         bytes calldata
     ) external override returns (bytes4) {
-        require(
-            totalLiquidity - uint256(-params.liquidityDelta) >= lockedLiquidity,
-            "Cannot remove locked liquidity"
-        );
+        // require(
+        //     totalLiquidity - uint256(-params.liquidityDelta) >= lockedLiquidity,
+        //     "Cannot remove locked liquidity"
+        // );
 
         if (params.liquidityDelta > 0) {
             totalLiquidity -= uint256(params.liquidityDelta);
@@ -177,7 +199,7 @@ contract PerpDexHook is BaseHook {
                 ),
                 "Transfer failed"
             );
-            traderCollateral[trader].currency0 += currency0;
+            traderCollateral[trader].currency0Amount += currency0;
         }
 
         if (currency1 > 0) {
@@ -189,7 +211,7 @@ contract PerpDexHook is BaseHook {
                 ),
                 "Transfer failed"
             );
-            traderCollateral[trader].currency1 += currency1;
+            traderCollateral[trader].currency1Amount += currency1;
         }
     }
 
@@ -212,96 +234,162 @@ contract PerpDexHook is BaseHook {
 
     // Trading functions
     function openPosition(
+        PoolKey calldata key,
+        address currencyBettingOn,
         uint256 marginAmount,
+        address marginCurrency,
         uint256 leverage,
         bool isLong
     ) external {
         require(leverage <= MAX_LEVERAGE, "Leverage too high");
-        uint256 positionSize = marginAmount * leverage;
-
-        // Check available liquidity
+        // FIX THIS ONE BELOW. TOTAL LIQUIDITY SHOULD BE THE RESERVERS OF TOKEN 0 AND TOKEN 1. LONG AND OPEN INTEREST ARE ALSO MORE FLUCTUATING DEPENDING ON PNL OR JUST MARGIN?
         require(
-            totalLiquidity - lockedLiquidity >= positionSize,
-            "Insufficient liquidity"
-        );
-        require(
-            traderCollateral[msg.sender] >= marginAmount,
-            "Insufficient trader collateral"
+            shortOpenInterest + longOpenInterest >= 2 * totalLiquidity,
+            "The size of open interest on this pool has been reached"
         );
         require(
             livePositions[msg.sender].trader == address(0),
             "A position already exists for this trader"
         );
 
-        int256 entryPrice = getCurrentPrice();
+        require(
+            currencyBettingOn != currency0Address ||
+                currencyBettingOn != currency1Address,
+            "Must bet on either currency0 or currency1"
+        );
+        require(
+            marginCurrency == currency0Address ||
+                marginCurrency == currency1Address,
+            "Insufficient currency0 or curency1 trader collateral"
+        );
+
+        require(
+            (marginCurrency == currency0Address &&
+                traderCollateral[msg.sender].currency0Amount >= marginAmount) ||
+                (marginCurrency == currency1Address &&
+                    traderCollateral[msg.sender].currency1Amount >=
+                    marginAmount),
+            "Insufficient currency0 or curency1 trader collateral"
+        );
+
+        uint256 currencyPrice = getCurrentPrice(key, marginCurrency);
+
+        // USD denominated position size
+        uint256 positionSize = currencyPrice * marginAmount * leverage;
 
         // Update position tracking
         livePositions[msg.sender] = Position({
             trader: msg.sender,
-            size: positionSize,
-            margin: marginAmount,
+            sizeUSD: positionSize,
+            marginAmount: marginAmount,
+            marginCurrency: marginCurrency,
+            currencyBettingOn: currencyBettingOn,
             leverage: leverage,
-            entryPrice: entryPrice,
+            entryPriceUSD: currencyPrice,
             isLong: isLong,
             timestamp: block.timestamp
         });
 
-        // Update global state
-        lockedLiquidity += positionSize;
         if (isLong) {
             longOpenInterest += positionSize;
         } else {
             shortOpenInterest += positionSize;
         }
 
-        traderCollateral[msg.sender] -= marginAmount;
+        if (marginCurrency == currency0Address) {
+            traderCollateral[msg.sender].currency0Amount -= marginAmount;
+        } else {
+            traderCollateral[msg.sender].currency1Amount -= marginAmount;
+        }
 
         emit PositionOpened(
             msg.sender,
-            entryPrice,
+            marginCurrency,
+            marginAmount,
+            currencyBettingOn,
+            currencyPrice,
             positionSize,
             leverage,
             isLong
         );
     }
 
-    function closePosition(address traderAddress) external {
+    function closePosition(
+        address traderAddress,
+        PoolKey calldata key
+    ) external {
         Position memory position = livePositions[traderAddress];
-        uint256 entryPrice = uint256(position.entryPrice);
-        uint256 exitPrice = uint256(getCurrentPrice());
-        uint256 originalPositionSize = position.margin * position.leverage;
+        uint256 currency0Price = getCurrentPrice(key, currency0Address);
+        uint256 currency1Price = getCurrentPrice(key, currency1Address);
 
-        uint256 currPositionSize;
+        uint256 entryPrice = position.entryPriceUSD;
+        uint256 exitPrice = position.currencyBettingOn == currency0Address
+            ? currency0Price
+            : currency1Price;
+
         uint256 profit;
 
-        if (exitPrice >= entryPrice && position.isLong) {
-            uint256 priceIncrease = exitPrice - entryPrice;
-            profit = (priceIncrease * originalPositionSize) / entryPrice;
+        bool marginCurrencyIs0 = currency0Address == position.marginCurrency
+            ? true
+            : false;
 
-            traderPnLFees -= profit;
-
-            currPositionSize = originalPositionSize + profit;
-        } else if (position.isLong) {
-            uint256 priceDecrease = exitPrice - entryPrice;
-            profit = (priceDecrease * originalPositionSize) / entryPrice;
-
-            traderPnLFees += profit;
-
-            currPositionSize = originalPositionSize - profit;
+        // Longs logic for payouts
+        if (position.isLong) {
+            uint256 priceShift = exitPrice - entryPrice;
+            profit = (priceShift * position.sizeUSD) / entryPrice;
+        } else {
+            uint256 priceShift = entryPrice - exitPrice;
+            profit = (priceShift * position.sizeUSD) / entryPrice;
         }
 
-        require(
-            totalLiquidity - lockedLiquidity >= currPositionSize,
-            "Insufficient liquidity"
-        );
+        if (profit > 0) {
+            // Add collateral back + increase collateral with profits. Also lock up the liquidity for trader payouts.
+            if (marginCurrencyIs0) {
+                uint256 currency0ToPayOut = profit / currency0Price;
 
-        traderCollateral[msg.sender] += position.margin + profit;
+                traderPayOutLockedCurrency0 += currency0ToPayOut;
 
-        lockedLiquidity -= originalPositionSize;
+                traderCollateral[msg.sender].currency0Amount +=
+                    position.marginAmount +
+                    currency0ToPayOut;
+            } else {
+                uint256 currency1ToPayOut = profit / currency1Price;
+
+                traderPayOutLockedCurrency1 += currency1ToPayOut;
+
+                traderCollateral[msg.sender].currency1Amount +=
+                    position.marginAmount +
+                    currency1ToPayOut;
+            }
+        }
+
+        if (profit < 0) {
+            // Take out collateral and losses and add it to pools fees.
+            if (marginCurrencyIs0) {
+                uint256 currency0ToTakeOut = profit / currency0Price;
+
+                traderCollateral[msg.sender]
+                    .currency0Amount -= currency0ToTakeOut;
+
+                traderPnLFeesCurrency0 +=
+                    position.marginAmount +
+                    currency0ToTakeOut;
+            } else {
+                uint256 currency1ToTakeOut = profit / currency1Price;
+
+                traderCollateral[msg.sender]
+                    .currency1Amount -= currency1ToTakeOut;
+
+                traderPnLFeesCurrency1 +=
+                    position.marginAmount +
+                    currency1ToTakeOut;
+            }
+        }
+
         if (position.isLong) {
-            longOpenInterest -= originalPositionSize;
+            longOpenInterest -= position.sizeUSD;
         } else {
-            shortOpenInterest -= originalPositionSize;
+            shortOpenInterest -= position.sizeUSD;
         }
 
         delete livePositions[msg.sender];
@@ -309,17 +397,22 @@ contract PerpDexHook is BaseHook {
         emit PositionClosed(
             msg.sender,
             position.isLong,
-            int256(exitPrice),
+            position.currencyBettingOn,
+            position.entryPriceUSD,
+            exitPrice,
             profit
         );
     }
 
     // This fn has to take into account collateral given by trader. Since if they have a ton of collateral, we shouldnt just close positions?
-    function liquidatePosition(address traderAddress) external {
+    function liquidatePosition(
+        address traderAddress,
+        PoolKey calldata key
+    ) external {
         Position memory position = livePositions[traderAddress];
         require(position.trader != address(0), "Position doesn't exist");
 
-        int256 currentPrice = getCurrentPrice();
+        // int256 currentPrice = getCurrentPrice();
         // int256 pnl = calculatePnL(position, currentPrice);
         // require(
         //     pnl <= -int256((position.margin * LIQUIDATION_THRESHOLD) / 100),
@@ -330,31 +423,58 @@ contract PerpDexHook is BaseHook {
         // TODO: Implementation
     }
 
-    // Helper functions
-    function getCurrentPrice() internal view returns (int256) {
-        // TODO: Implementation
-        return 0;
+    function getCurrentPrice(
+        PoolKey calldata key,
+        address currencyAddress
+    ) internal view returns (uint256) {
+        (, int256 chainlinkBasePrice, , , ) = priceFeed.latestRoundData();
+        require(chainlinkBasePrice > 0, "Invalid base price from Chainlink");
+
+        if (
+            (isBaseCurrency0 && currencyAddress == currency0Address) ||
+            (!isBaseCurrency0 && currencyAddress == currency1Address)
+        ) {
+            return uint256(chainlinkBasePrice);
+        }
+
+        (uint160 sqrtPriceX96, , , ) = poolManager.getSlot0(key.toId());
+        uint256 poolPrice = (uint256(sqrtPriceX96) * uint256(sqrtPriceX96)) >>
+            192;
+
+        if (isBaseCurrency0) {
+            // Chainlink base is currency0 (ETH)
+            if (currencyAddress == currency1Address) {
+                // Pool is Base/Quote (ETH/PEPE), poolPrice = PEPE per ETH
+                // PEPE/USD = ETH/USD / (PEPE/ETH)
+                return (uint256(chainlinkBasePrice) * 1e18) / poolPrice;
+            }
+        } else {
+            // Chainlink base is currency1 (PEPE)
+            if (currencyAddress == currency0Address) {
+                // Pool is Quote/Base (PEPE/ETH), poolPrice = ETH per PEPE
+                // PEPE/USD = ETH/USD * (ETH/PEPE)
+                return (uint256(chainlinkBasePrice) * poolPrice) / 1e18;
+            }
+        }
+
+        revert("Invalid currency address");
     }
 
     function getLeverageRate(
-        PoolKey calldata key,
         address traderAddress
     ) public view returns (uint256) {
         return
             (BASE_LEVERAGE_RATE *
                 livePositions[traderAddress].leverage *
-                (getUtilizationRate(key) * 10)) / 1e4;
+                (getUtilizationRate() * 10)) / 1e4;
     }
 
-    function getUtilizationRate(
-        PoolKey calldata key
-    ) public view returns (uint256) {
-        return (lockedLiquidity * 1e4) / totalLiquidity;
+    // FIX THIS WITH USD AND STUFF. USED UP RESERVERS DIVIDED BY TOTAL RESERVES.
+    function getUtilizationRate() public view returns (uint256) {
+        return (0 * 1e4) / totalLiquidity;
     }
 
-    function getImbalanceRate(
-        PoolKey calldata key
-    ) public view returns (uint256) {
+    function getImbalanceRate() public view returns (uint256) {
         return
             (longOpenInterest - shortOpenInterest) /
             (longOpenInterest + shortOpenInterest);
@@ -362,11 +482,18 @@ contract PerpDexHook is BaseHook {
 
     function validateFeed(address tokenAddress) internal view returns (bool) {
         try priceFeed.description() returns (string memory desc) {
-            bytes32 tokenSymbol = keccak256(abi.encodePacked(desc));
-            bytes32 addressSymbol = keccak256(
-                abi.encodePacked(IERC20Metadata(tokenAddress).symbol())
-            );
-            return tokenSymbol == addressSymbol;
+            string memory tokenSymbol = IERC20Metadata(tokenAddress).symbol();
+
+            // Convert strings to slices
+            strings.slice memory descSlice = desc.toSlice();
+            strings.slice memory delimiter = "/".toSlice();
+            strings.slice memory symbolSlice = tokenSymbol.toSlice();
+
+            strings.slice memory baseSlice = descSlice.split(delimiter);
+            strings.slice memory quoteSlice = descSlice;
+
+            return
+                symbolSlice.equals(baseSlice) || symbolSlice.equals(quoteSlice);
         } catch {
             return false;
         }
@@ -374,7 +501,10 @@ contract PerpDexHook is BaseHook {
 
     event PositionOpened(
         address indexed trader,
-        int256 entryPrice,
+        address marginCurrency,
+        uint256 marginAmount,
+        address currencyBettingOn,
+        uint256 entryPrice,
         uint256 size,
         uint256 leverage,
         bool isLong
@@ -382,7 +512,9 @@ contract PerpDexHook is BaseHook {
     event PositionClosed(
         address indexed trader,
         bool isLong,
-        int256 exitPrice,
+        address currencyBettingOn,
+        uint256 entryPrice,
+        uint256 exitPrice,
         uint256 profit
     );
     event PositionLiquidated(address indexed trader, uint256 margin);
